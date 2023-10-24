ff8b285e24c44ea0362b405635e7fb185a43020c
osparking <jbpark03@gmail.com>
2023-10-24 22:32:45 +0900

    Payment 대신 ResponseEntity 반환

--- src/main/java/sq_ch11_ex1/controller/PaymentsController.java
+++ src/main/java/sq_ch11_ex1/controller/PaymentsController.java
@@ -2,6 +2,7 @@ package sq_ch11_ex1.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class PaymentsController {
 	private final PaymentsProxy proxy;
 	
 	@PostMapping("/payment")
-	public Payment processPayment(@RequestBody Payment payment) {
+	public ResponseEntity<Payment> processPayment(@RequestBody Payment payment) {
 		String requestId = UUID.randomUUID().toString();
 		return proxy.createPayment(requestId, payment);
 	}
--- src/main/java/sq_ch11_ex1/proxy/PaymentsProxy.java
+++ src/main/java/sq_ch11_ex1/proxy/PaymentsProxy.java
@@ -1,6 +1,7 @@
 package sq_ch11_ex1.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,7 +12,7 @@ import sq_ch11_ex1.dto.Payment;
 						url = "${name.service.url}")
 public interface PaymentsProxy {
 	@PostMapping("/payment")
-	Payment createPayment(
+	ResponseEntity<Payment> createPayment(
 			@RequestHeader String requestId,
 			@RequestBody Payment payment);
 }
